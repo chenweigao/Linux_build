58c57b357efa926833e2fdafd0d9734223551745
chenweigao <297859260@qq.com>
2018-04-12 22:29:24 +0800

    add choice for install vscode

--- onekey_setup.sh
+++ onekey_setup.sh
@@ -46,8 +46,13 @@ Test_And_Install_Package vim
 Test_And_Install_Package python-pip
 Test_And_Install_Package guake
 
+read -p "Would you like to install Visual Studio Code? 'Y' or 'N' :" answer
+case answer in
+Y|y) 
 mkdir /tmp/vscode
 cd /tmp/vscode/
 wget https://vscode.cdn.azure.cn/stable/950b8b0d37a9b7061b6f0d291837ccc4015f5ecd/code_1.22.1-1522974421_amd64.deb
 sudo dpkg -i *.deb
-cd ~
+cd ~;;
+N|n)
+echo "You will not install Visual Studio Code";;
